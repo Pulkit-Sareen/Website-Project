2d4e68a13d885c81b8335a2f0f2f38727ab41403
Pulkit <pulkitsareen2006@gmail.com>
2024-04-24 04:12:46 +0530

    responsive navbar

---
+++ script.js
@@ -0,0 +1,9 @@
+// code to remove and show sidebar
+function sidebarPopUp(){
+    const sidebar =document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function sidebarClose(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
