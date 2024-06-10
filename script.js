4d584207719d43a3e7e2fc553ca87bc4266da871
ManraiSingh <smanraisingh@gmail.com>
2024-06-10 20:06:57 +0530

    ADDED NEW INDEX FILE

--- script.js
+++
@@ -1,9 +0,0 @@
-// code to remove and show sidebar
-function sidebarPopUp(){
-    const sidebar =document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-}
-function sidebarClose(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-}
